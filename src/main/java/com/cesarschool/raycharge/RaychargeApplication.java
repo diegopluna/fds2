4f3f51dc5fdf98726119b362fa904ece4d7abcd5
Diego Luna <dpl@cesar.school>
2024-09-12 08:32:58 -0300

    chore: setup project

---
+++ src/main/java/com/cesarschool/raycharge/RaychargeApplication.java
@@ -0,0 +1,13 @@
+package com.cesarschool.raycharge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaychargeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaychargeApplication.class, args);
+	}
+
+}
