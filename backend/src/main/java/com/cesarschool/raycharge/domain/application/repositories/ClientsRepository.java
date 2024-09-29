2683bbc1a585ce2c17dfc135dee58e384203fabc
Diego Peter <dpl@cesar.school>
2024-09-29 14:47:52 -0300

    reestruturacao

--- backend/src/main/java/com/cesarschool/raycharge/domain/application/repositories/ClientsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cesarschool.raycharge.domain.application.repositories;
-
-import com.cesarschool.raycharge.domain.enterprise.entities.Client;
-
-public interface ClientsRepository {
-  public void create(Client client);
-}
