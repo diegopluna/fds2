d2e003f7add53084089b5a831a9685e32f120b43
Virna Amaral <vfpa@cesar.school>
2024-11-01 19:07:58 -0300

    ajustes nos popups de cancelamento de agendamento

--- frontend/src/components/popup-cancelamento-concluido.tsx
+++ frontend/src/components/popup-cancelamento-concluido.tsx
@@ -8,11 +8,11 @@ const CancelamentoConcluidoMotoristaModal: React.FC<CancelamentoConcluidoMotoris
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
       <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
-        <h2 className="text-xl font-bold text-center mb-4">Seu agendamento foi <span className="text-red-500">CANCELADO</span> com sucesso!</h2>
+        <h2 className="text-2xl font-bold text-center mb-8 mt-2 ml-2 mr-2">Seu agendamento foi <span className="text-red-500">CANCELADO</span> com sucesso!</h2>
         <div className="flex justify-center mt-6">
           <button
             onClick={onClose}
-            className="bg-green-500 text-white px-6 py-2 rounded-md hover:bg-green-600"
+            className="bg-green-500 text-white px-16 py-2 rounded-md hover:bg-green-600"
           >
             Concluir
           </button>
